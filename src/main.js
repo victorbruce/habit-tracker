957d3b40d4e0250f5a7dd11bde81c6ad55c186f3
Victor Bruce <victorbruce82@gmail.com>
2025-05-09 04:06:44 +0000

    feat: add form data to localstorage

--- src/main.js
+++ src/main.js
@@ -1,6 +1,22 @@
-import { setupDropdown } from "./components/dropdown";
+import { setupDropdown } from "./components/dropdown.js";
 
+console.log("hello world");
 const dropdownButton = document.getElementById("dropdownButton");
 const dropdownMenu = document.getElementById("dropdownMenu");
 
 setupDropdown(dropdownButton, dropdownMenu);
+
+const habitForm = document.getElementById("habitForm");
+
+habitForm.addEventListener("submit", (event) => {
+  event.preventDefault(); // prevents page from reloading
+
+  // get input element
+  const input = document.getElementById("notes");
+  const value = input.value;
+
+  localStorage.setItem("habit", value);
+
+  alert("saved: " + value);
+  input.value = "";
+});
