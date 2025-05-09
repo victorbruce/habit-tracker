ae8af94c97d5c5a3068a3151b5016a74f4ec9264
Victor Bruce <victorbruce82@gmail.com>
2025-05-09 10:17:45 +0000

    feat: function to select an activity and set the global activity state variable

---
+++ src/state/state.js
@@ -0,0 +1,9 @@
+let selectedActivity = null;
+
+export function setSelectedActivity(activity) {
+  selectedActivity = activity;
+}
+
+export function getSelectedActivity() {
+  return selectedActivity;
+}
