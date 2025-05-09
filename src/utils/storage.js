3ea976a4ff44a450fd51c02b5c0743eb3381359d
Victor Bruce <victorbruce82@gmail.com>
2025-05-09 16:29:41 +0000

    feat: add the functionality to delete an habit

--- src/utils/storage.js
+++ src/utils/storage.js
@@ -1,7 +1,14 @@
+const HABIT_KEY = "habits";
+
 export function saveHabits(habits) {
   localStorage.setItem("habits", JSON.stringify(habits));
 }
 
 export function getHabits() {
   return JSON.parse(localStorage.getItem("habits")) || [];
+}
+
+export function deleteHabitById(id) {
+  const habits = getHabits().filter(h => h.id !== id);
+  localStorage.setItem(HABIT_KEY, JSON.stringify(habits));
 }
