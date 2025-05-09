0e6a06bd4be7204b4765ec302ee1a5faacfe3820
Victor Bruce <victorbruce82@gmail.com>
2025-05-09 10:19:39 +0000

    refactor: extend the object added to local storage when adding habits

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 import { setupDropdown } from "./components/dropdown.js";
 import { addHabitForm } from "./components/addHabitForm.js";
+import { setupActivitySelection } from "./components/activitySelector.js";
 
 const dropdownButton = document.getElementById("dropdownButton");
 const dropdownMenu = document.getElementById("dropdownMenu");
 const noteInputField = document.getElementById("notes");
 const habitForm = document.getElementById("habitForm");
+const activityList = document.querySelector(".habit__list");
 
 setupDropdown(dropdownButton, dropdownMenu);
-addHabitForm(habitForm, noteInputField, 'habit');
+setupActivitySelection(activityList);
+addHabitForm(habitForm, noteInputField, "habit");
